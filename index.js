ca57361b8c0be4e13ed067e1d1e36dc32a144c38
tn9014755-spec <tn9014755@gmail.com>
2026-08-15 20:04:39 +0700

    Enable BC FRESH LINE workflow

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-// BC NGAY entrypoint for the existing Render service.
+// BC NGAY + BC FRESH entrypoint for the existing Render service.
+// BC FRESH patch is loaded first so it can extend the existing LINE webhook.
+require('./bc_fresh_bot.js');
 require('./bc_server.js');
